13af94b508c30b3c628ea32209f8d3f16d48bd32
Gautam B <t-gauba@microsoft.com>
2016-04-10 12:29:45 +0530

    Obtaining user feed, photo and friend list from Facebook

--- app/controllers/recommender.server.controller.js
+++ app/controllers/recommender.server.controller.js
@@ -3,12 +3,48 @@
  */
 var fetcher = require('./fb.datafetch.server.controller');
 
+exports.getUserFeed = function (userData)
+{
+    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/feed', function (buffer)
+    {
+        console.log('User feed:');
+        console.log(buffer);
+    });
+};
 
 exports.getUserProfile = function (userData)
 {
     fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me?fields=bio,birthday,email,hometown,link,locale', function (buffer)
     {
         console.log('User profile:');
+        console.log(buffer);
+    });
+};
+
+
+exports.getUserPhoto = function (userData)
+{
+    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/photos', function (buffer)
+    {
+        console.log('User photo:');
+        console.log(buffer);
+    });
+};
+
+exports.getFriendsList = function (userData)
+{
+    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/invitable_friends', function (buffer)
+    {
+        console.log('Friends list:');
+        console.log(buffer);
+    });
+};
+
+exports.getUserLikes = function (userData)
+{
+    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/likes', function (buffer)
+    {
+        console.log('User likes:');
         console.log(buffer);
     });
 };
