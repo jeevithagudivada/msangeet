9b495c61d0d0db27e9df3db4b07acc25c5d5301c
Gautam B <t-gauba@microsoft.com>
2016-04-10 12:24:56 +0530

    Obtaining user profile from Facebook

--- app/controllers/recommender.server.controller.js
+++ app/controllers/recommender.server.controller.js
@@ -1,33 +1,14 @@
 /**
  * Created by gauth_000 on 02-Apr-16.
  */
-var fetcher=require('./fb.datafetch.server.controller');
-exports.getUserFeed = function (userData)
+var fetcher = require('./fb.datafetch.server.controller');
+
+
+exports.getUserProfile = function (userData)
 {
-    console.log('User feed:');
-    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/feed', onDataFetchComplete);
+    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me?fields=bio,birthday,email,hometown,link,locale', function (buffer)
+    {
+        console.log('User profile:');
+        console.log(buffer);
+    });
 };
-
-exports.getUserPhoto= function (userData)
-{
-    console.log('User photo:');
-    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/photos', onDataFetchComplete);
-};
-
-exports.getFriendsList=function(userData)
-{
-    console.log('Friends list:');
-    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/invitable_friends', onDataFetchComplete);
-};
-
-exports.getUserLikes=function(userData)
-{
-    console.log('User likes:');
-    fetcher.getFBData(userData.providerData.accessToken, '/v2.5/me/likes', onDataFetchComplete);
-};
-
-
-function onDataFetchComplete(buffer)
-{
-    console.log(buffer);
-}
